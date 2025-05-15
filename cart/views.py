7a1c03ae1a8f0480a17e117f79f59f831cc99d6d
Randysman <134296647+Randysman@users.noreply.github.com>
2025-05-15 18:59:29 +0300

    create app cart

---
+++ cart/views.py
@@ -0,0 +1,46 @@
+from zoneinfo import available_timezones
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart
+from django.views import View
+from ..main.models import ClothingItem, ClothingItemSize, Size
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request,'cart/cart_detail.html', {'cart': cart})
+
+
+def cart_add(request, item_id):
+    cart = Cart(request)
+    clothing_item = get_object_or_404(ClothingItem, id=item_id)
+    size = request.POST.get('size')
+
+    if size:
+        try:
+            size_obj = Size.objects.get(name=size)
+            clothing_item_size = ClothingItemSize.objects.get(clothing_item=clothing_item, size=size_obj)
+
+            if not clothing_item_size.available:
+                return redirect('cart:cart_detail')
+        except Size.DoesNotExist:
+            return redirect('cart:cart_detail')
+        except ClothingItemSize.DoesNotExitst:
+            return redirect('cart:cart_detail')
+    else:
+        available_sizes = clothing_item.sizes.filter(clothingitemsize__available=True)
+        if available_sizes.exists():
+            size_obj = available_sizes.first()
+            size = size_obj.name
+        else:
+            return redirect('cart:cart_detail')
+
+    cart.add(clothing_item, size)
+    return redirect('cart:cart_detail')
+
+def cart_remove(request, item_id):
+    cart = Cart(request)
+    clothing_item = get_object_or_404(ClothingItem, id=item_id)
+    cart.remove(clothing_item)
+    return redirect('cart:cart_detail')
+
