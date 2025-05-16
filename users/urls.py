1b1e852515d22da1b7a718a8d8975c27a9c6ce8e
Randysman <134296647+Randysman@users.noreply.github.com>
2025-05-16 17:59:08 +0300

    create urls and html page login

--- users/urls.py
+++ users/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
-from .views import
+from . import views
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.logout, name='logout'),
+]
